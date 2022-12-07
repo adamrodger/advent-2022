f54142291a38f66a485a6dea82c95f24ba787978
Adam Rodger <adam.rodger@gmail.com>
2022-12-07 08:10:27 +0000

    day(7): No Space Left On Device :cookie:

---
+++ src/AdventOfCode/Day7.cs
@@ -0,0 +1,105 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 7
+    /// </summary>
+    public class Day7
+    {
+        public int Part1(string[] input)
+        {
+            Dictionary<string, Directory> filesystem = BuildFilesystem(input);
+
+            return filesystem.Values.Select(dir => dir.TotalSize()).Where(size => size <= 100_000).Sum();
+        }
+
+        public int Part2(string[] input)
+        {
+            Dictionary<string, Directory> filesystem = BuildFilesystem(input);
+
+            int currentSize = filesystem["/"].TotalSize();
+            int freeSpace = 70000000 - currentSize;
+            int required = 30000000 - freeSpace;
+
+            return filesystem.Values.Select(dir => dir.TotalSize()).Where(size => size >= required).Min();
+        }
+
+        private static Dictionary<string, Directory> BuildFilesystem(string[] input)
+        {
+            var filesystem = new Dictionary<string, Directory>();
+            var stack = new Stack<Directory>();
+            stack.Push(new Directory { Path = "/" });
+            filesystem["/"] = stack.Peek();
+
+            foreach (string line in input.Skip(1))
+            {
+                if (line.StartsWith("$ cd .."))
+                {
+                    stack.Pop();
+                }
+                else if (line.StartsWith("$ cd"))
+                {
+                    string name = line[5..];
+                    string path = stack.Peek().Path + "/" + name;
+                    stack.Push(filesystem[path]);
+                }
+                else if (line.StartsWith("$ ls"))
+                {
+                    continue;
+                }
+                else if (line.StartsWith("dir"))
+                {
+                    var current = stack.Peek();
+                    var sub = current.AddSubdirectory(line[4..]);
+                    filesystem[sub.Path] = sub;
+                }
+                else // file
+                {
+                    string[] parts = line.Split(' ');
+                    int size = int.Parse(parts[0]);
+                    string name = parts[1];
+
+                    var current = stack.Peek();
+                    current.AddFile(name, size);
+                }
+            }
+
+            return filesystem;
+        }
+
+        public class Directory
+        {
+            private readonly ICollection<Directory> subDirectories = new List<Directory>();
+            private readonly ICollection<File> files = new List<File>();
+
+            public string Path { get; init; }
+
+            public Directory AddSubdirectory(string name)
+            {
+                var sub = new Directory { Path = this.Path + "/" + name };
+                this.subDirectories.Add(sub);
+                return sub;
+            }
+
+            public File AddFile(string name, int size)
+            {
+                var file = new File { Path = this.Path + "/" + name, Size = size};
+                this.files.Add(file);
+                return file;
+            }
+
+            public int TotalSize()
+            {
+                return this.subDirectories.Select(s => s.TotalSize()).Sum() + this.files.Select(f => f.Size).Sum();
+            }
+        }
+
+        public class File
+        {
+            public string Path { get; init; }
+            public int Size { get; init; }
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day7Tests.cs
@@ -0,0 +1,47 @@
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day7Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day7 solver;
+
+        public Day7Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day7();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day7.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 1453349;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 7 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 2948823;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 7 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
