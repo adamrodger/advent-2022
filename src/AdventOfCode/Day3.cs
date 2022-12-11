6e779f01e43a28d8f245dac843cce6ed82c9349a
Adam Rodger <adam.rodger@gmail.com>
2022-12-11 15:08:03 +0000

    perf(3): Stop at first intersection

--- src/AdventOfCode/Day3.cs
+++ src/AdventOfCode/Day3.cs
@@ -15,13 +15,10 @@ namespace AdventOfCode
 
             foreach (string line in input)
             {
-                HashSet<char> first = line[..(line.Length/2)].ToHashSet();
-                HashSet<char> second = line[(line.Length / 2)..].ToHashSet();
+                IEnumerable<char> first = line[..(line.Length/2)];
+                IEnumerable<char> second = line[(line.Length / 2)..];
 
-                first.IntersectWith(second);
-
-                Debug.Assert(first.Count == 1);
-                char common = first.First();
+                char common = first.Intersect(second).First();;
 
                 if (common is >= 'a' and <= 'z')
                 {
@@ -42,15 +39,11 @@ namespace AdventOfCode
 
             foreach (string[] group in input.Chunk(3))
             {
-                HashSet<char> first = group[0].ToHashSet();
-                HashSet<char> second = group[1].ToHashSet();
-                HashSet<char> third = group[2].ToHashSet();
+                IEnumerable<char> first = group[0];
+                IEnumerable<char> second = group[1];
+                IEnumerable<char> third = group[2];
 
-                first.IntersectWith(second);
-                first.IntersectWith(third);
-
-                Debug.Assert(first.Count == 1);
-                char common = first.First();
+                char common = first.Intersect(second).Intersect(third).First();
 
                 if (common is >= 'a' and <= 'z')
                 {
