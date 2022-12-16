40c69061fb20c90eb6dafcbad88a269036019174
Adam Rodger <adam.rodger@gmail.com>
2022-12-16 13:38:00 +0000

    going mad

--- src/AdventOfCode/Day16.cs
+++ src/AdventOfCode/Day16.cs
@@ -24,59 +24,230 @@ namespace AdventOfCode
                 valves[id] = new Valve(id, rate, paths);
             }
 
-            Queue<GameState> queue = new Queue<GameState>();
-            queue.Enqueue(new GameState("AA", 30, 0, new HashSet<string>()));
+            PriorityQueue<GameState, int> queue = new();
+            queue.Enqueue(new GameState("AA", 30, 0, string.Empty), 0);
+
+            HashSet<GameState> visited = new();
 
             int maxPressure = int.MinValue;
 
-            while (queue.TryDequeue(out GameState state))
+            while (queue.TryDequeue(out GameState state, out int priority))
             {
                 if (state.TimeRemaining < 1)
                 {
+                    //return state.PressureReleased;
                     maxPressure = Math.Max(state.PressureReleased, maxPressure);
                     continue;
                 }
 
-                Valve valve = valves[state.Current];
-
-                if (!state.OpenValves.Contains(valve.Id))
+                if (visited.Contains(state))
                 {
-                    queue.Enqueue(state with
-                    {
-                        TimeRemaining = state.TimeRemaining - 1,
-                        OpenValves = new HashSet<string>(state.OpenValves) { valve.Id } // inefficient copying of set every time we queue
-                    });
+                    continue;
                 }
 
-                // release pressure - inefficient because we're doing this ever step, could multiply up front
-                int pressure = state.PressureReleased + state.OpenValves.Select(v => valves[v].Rate).Sum();
+                visited.Add(state);
 
+                Valve valve = valves[state.Current];
+
+                if (!state.OpenValves.Contains(valve.Id) && valve.Rate > 0)
+                {
+                    // open the valve
+                    GameState nextState = state with
+                    {
+                        TimeRemaining = state.TimeRemaining - 1,
+                        OpenValves = string.Join(",", state.OpenValves.Split(',').Append(valve.Id).OrderBy(v => v)),
+                        PressureReleased = state.PressureReleased + ((state.TimeRemaining - 1) * valve.Rate)
+                    };
+
+                    queue.Enqueue(nextState, nextState.PressureReleased * -1);
+                }
+
+                // move to all the other valves
                 foreach (string other in valve.Paths)
                 {
-                    queue.Enqueue(state with
+                    GameState nextState = state with
                     {
                         Current = other,
+                        TimeRemaining = state.TimeRemaining - 1
+                    };
+
+                    queue.Enqueue(nextState, nextState.PressureReleased * -1);
+                }
+            }
+
+            return maxPressure;
+            throw new InvalidOperationException("Never got to the end somehow");
+        }
+
+        public int Part2(string[] input)
+        {
+            /*IDictionary<string, Valve> valves = new Dictionary<string, Valve>(input.Length);
+
+            foreach (string line in input)
+            {
+                // Valve GJ has flow rate=14; tunnels lead to valves UV, AO, MM, UD, GM
+                string[] parts = line.Split(' ');
+                string id = parts[1];
+                int rate = int.Parse(parts[4][5..^1]);
+                string[] paths = parts[9..].Select(p => p.Replace(",", string.Empty)).ToArray();
+
+                valves[id] = new Valve(id, rate, paths);
+            }
+
+            PriorityQueue<GameState2, int> queue = new();
+            queue.Enqueue(new GameState2("AA", "AA", 26, 0, string.Empty), 0);
+
+            HashSet<GameState2> visited = new();
+
+            int maxPressure = int.MinValue;
+
+            while (queue.TryDequeue(out GameState2 state, out int priority))
+            {
+                if (state.TimeRemaining < 1)
+                {
+                    //return state.PressureReleased;
+                    maxPressure = Math.Max(state.PressureReleased, maxPressure);
+                    continue;
+                }
+
+                if (visited.Contains(state))
+                {
+                    continue;
+                }
+
+                visited.Add(state);
+
+                Valve myValve = valves[state.Current];
+                Valve elephantValve = valves[state.Elephant];
+
+                if (!state.OpenValves.Contains(myValve.Id) && myValve.Rate > 0)
+                {
+                    // I open the valve whilst the elephant moves
+                    GameState2 nextState = state with
+                    {
                         TimeRemaining = state.TimeRemaining - 1,
-                        PressureReleased = pressure
-                    });
+                        OpenValves = string.Join(",", state.OpenValves.Split(',').Append(myValve.Id).OrderBy(v => v)),
+                        PressureReleased = state.PressureReleased + ((state.TimeRemaining - 1) * myValve.Rate)
+                    };
+
+                    foreach (string other in elephantValve.Paths)
+                    {
+                        queue.Enqueue(nextState with { Elephant = other }, nextState.PressureReleased * -1);
+                    }
+                }
+
+                if (!state.OpenValves.Contains(elephantValve.Id) && elephantValve.Rate > 0)
+                {
+                    // elephant opens the valve whilst I move
+                    GameState2 nextState = state with
+                    {
+                        TimeRemaining = state.TimeRemaining - 1,
+                        OpenValves = string.Join(",", state.OpenValves.Split(',').Append(elephantValve.Id).OrderBy(v => v)),
+                        PressureReleased = state.PressureReleased + ((state.TimeRemaining - 1) * elephantValve.Rate)
+                    };
+
+                    foreach (string other in myValve.Paths)
+                    {
+                        queue.Enqueue(nextState with { Current = other }, nextState.PressureReleased * -1);
+                    }
+                }
+
+                // we both move
+                foreach (string other in myValve.Paths)
+                {
+                    foreach (string eOther in elephantValve.Paths)
+                    {
+                        GameState2 nextState = state with
+                        {
+                            Current = other,
+                            Elephant = eOther,
+                            TimeRemaining = state.TimeRemaining - 1
+                        };
+
+                        queue.Enqueue(nextState, nextState.PressureReleased * -1);
+                    }
+                }
+            }
+
+            return maxPressure;*/
+
+            IDictionary<string, Valve> valves = new Dictionary<string, Valve>(input.Length);
+
+            foreach (string line in input)
+            {
+                // Valve GJ has flow rate=14; tunnels lead to valves UV, AO, MM, UD, GM
+                string[] parts = line.Split(' ');
+                string id = parts[1];
+                int rate = int.Parse(parts[4][5..^1]);
+                string[] paths = parts[9..].Select(p => p.Replace(",", string.Empty)).ToArray();
+
+                valves[id] = new Valve(id, rate, paths);
+            }
+
+            PriorityQueue<GameState2, int> queue = new();
+            queue.Enqueue(new GameState2("AA", 26, 0, string.Empty, false), 0);
+
+            HashSet<GameState2> visited = new();
+
+            int maxPressure = int.MinValue;
+
+            while (queue.TryDequeue(out GameState2 state, out int priority))
+            {
+                if (state.TimeRemaining < 1)
+                {
+                    //return state.PressureReleased;
+                    maxPressure = Math.Max(state.PressureReleased, maxPressure);
+
+                    if (!state.ElephantTurn)
+                    {
+                        // now do the elephant
+                        queue.Enqueue(new GameState2("AA", 26, state.PressureReleased, state.OpenValves, true), state.PressureReleased * -1);
+                    }
+
+                    continue;
+                }
+
+                if (visited.Contains(state))
+                {
+                    continue;
+                }
+
+                visited.Add(state);
+
+                Valve valve = valves[state.Current];
+
+                if (!state.OpenValves.Contains(valve.Id) && valve.Rate > 0)
+                {
+                    // open the valve
+                    GameState2 nextState = state with
+                    {
+                        TimeRemaining = state.TimeRemaining - 1,
+                        OpenValves = string.Join(",", state.OpenValves.Split(',').Append(valve.Id).OrderBy(v => v)),
+                        PressureReleased = state.PressureReleased + ((state.TimeRemaining - 1) * valve.Rate)
+                    };
+
+                    queue.Enqueue(nextState, nextState.PressureReleased * -1);
+                }
+
+                // move to all the other valves
+                foreach (string other in valve.Paths)
+                {
+                    GameState2 nextState = state with
+                    {
+                        Current = other,
+                        TimeRemaining = state.TimeRemaining - 1
+                    };
+
+                    queue.Enqueue(nextState, nextState.PressureReleased * -1);
                 }
             }
 
             return maxPressure;
         }
 
-        public int Part2(string[] input)
-        {
-            foreach (string line in input)
-            {
-                throw new NotImplementedException("Part 2 not implemented");
-            }
-
-            return 0;
-        }
-
         private record Valve(string Id, int Rate, ICollection<string> Paths);
 
-        private record GameState(string Current, int TimeRemaining, int PressureReleased, ISet<string> OpenValves);
+        private record GameState(string Current, int TimeRemaining, int PressureReleased, string OpenValves);
+        private record GameState2(string Current, int TimeRemaining, int PressureReleased, string OpenValves, bool ElephantTurn);
     }
 }
