93a31414f1123eea0feb27d3ebba148ab16f5627
Adam Rodger <adam.rodger@gmail.com>
2022-12-22 16:17:22 +0000

    day(22): Monkey Map :snowman:

---
+++ src/AdventOfCode/Day22.cs
@@ -0,0 +1,306 @@
+using System;
+using System.Collections.Generic;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 22
+    /// </summary>
+    public class Day22
+    {
+        private const char Open = '.';
+        private const char Wall = '#';
+        private const char Blank = ' ';
+
+        public int Part1(string[] input)
+        {
+            char[,] grid = input[..^2].ToGrid();
+
+            IList<Instruction> instructions = Parse(input[^1]);
+
+            Point2D location = (input[0].IndexOf(Open), 0);
+            Bearing bearing = Bearing.East;
+
+            foreach (Instruction instruction in instructions)
+            {
+                if (instruction is Turn turn)
+                {
+                    bearing = bearing.Turn(turn.Direction);
+                    continue;
+                }
+
+                var steps = instruction as Step ?? throw new ArgumentOutOfRangeException();
+
+                for (int i = 0; i < steps.Count; i++)
+                {
+                    Point2D nextLocation = Move(location, bearing, grid);
+                    char nextSquare = grid[nextLocation.Y, nextLocation.X];
+
+                    if (nextSquare == Open)
+                    {
+                        location = nextLocation;
+                    }
+                    else if (nextSquare == Wall)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        // teleport round to the other side
+                        while (nextSquare == Blank)
+                        {
+                            nextLocation = Move(nextLocation, bearing, grid);
+                            nextSquare = grid[nextLocation.Y, nextLocation.X];
+                        }
+
+                        if (nextSquare == Wall)
+                        {
+                            break;
+                        }
+
+                        location = nextLocation;
+                    }
+                }
+            }
+
+            return CalculateScore(bearing, location);
+        }
+
+        public int Part2(string[] input)
+        {
+            char[,] grid = input[..^2].ToGrid();
+
+            IList<Instruction> instructions = Parse(input[^1]);
+
+            Point2D location = (input[0].IndexOf(Open), 0);
+            Bearing bearing = Bearing.East;
+
+            foreach (Instruction instruction in instructions)
+            {
+                if (instruction is Turn turn)
+                {
+                    bearing = bearing.Turn(turn.Direction);
+                    continue;
+                }
+
+                var steps = instruction as Step ?? throw new ArgumentOutOfRangeException();
+
+                for (int i = 0; i < steps.Count; i++)
+                {
+                    (Point2D nextLocation, Bearing nextBearing) = MoveCube(location, bearing, grid);
+                    char nextSquare = grid[nextLocation.Y, nextLocation.X];
+
+                    if (nextSquare == Open)
+                    {
+                        location = nextLocation;
+                        bearing = nextBearing;
+                    }
+                    else if (nextSquare == Wall)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Tried to step off the cube");
+                    }
+                }
+            }
+            
+            return CalculateScore(bearing, location);
+        }
+
+        /// <summary>
+        /// Parse the input
+        /// </summary>
+        /// <param name="input">Input</param>
+        /// <returns>A list of parsed instructions</returns>
+        private static IList<Instruction> Parse(string input)
+        {
+            var instructions = new List<Instruction>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!char.IsAsciiDigit(input[i]))
+                {
+                    TurnDirection direction = input[i] switch
+                    {
+                        'L' => TurnDirection.Left,
+                        'R' => TurnDirection.Right,
+                        _ => throw new ArgumentOutOfRangeException()
+                    };
+
+                    instructions.Add(new Turn(direction));
+                }
+                else
+                {
+                    int length = 0;
+
+                    while (i + length < input.Length && char.IsAsciiDigit(input[i + length]))
+                    {
+                        length++;
+                    }
+
+                    int steps = int.Parse(input.Substring(i, length));
+                    instructions.Add(new Step(steps));
+
+                    i += length - 1;
+                }
+            }
+
+            return instructions;
+        }
+
+        /// <summary>
+        /// Work out where we'd move to when stepping around the grid as a 2D map
+        /// </summary>
+        /// <param name="current">Current location</param>
+        /// <param name="bearing">Current bearing</param>
+        /// <param name="grid">Grid</param>
+        /// <returns>Potential next location</returns>
+        private static Point2D Move(Point2D current, Bearing bearing, char[,] grid) => bearing switch
+        {
+            Bearing.North => (current.X, current.Y > 0 ? current.Y - 1 : grid.GetLength(0) - 1),
+            Bearing.South => (current.X, (current.Y + 1) % grid.GetLength(0)),
+            Bearing.East => ((current.X + 1) % grid.GetLength(1), current.Y),
+            Bearing.West => (current.X > 0 ? current.X - 1 : grid.GetLength(1) - 1, current.Y),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        /// <summary>
+        /// Work out where we'd move to and which direction e'd be facing when stepping around the map as a cube
+        ///
+        /// This can cause the bearing to change as you move from one cube face to another
+        /// </summary>
+        /// <param name="current">Current location on the 2D map</param>
+        /// <param name="bearing">Current bearing</param>
+        /// <param name="grid">2D map</param>
+        /// <returns>Next location and next bearing</returns>
+        public static (Point2D Next, Bearing Bearing) MoveCube(Point2D current, Bearing bearing, char[,] grid)
+        {
+            Point2D next = Move(current, bearing, grid);
+
+            if (grid[next.Y, next.X] != Blank)
+            {
+                // this is a valid move - either we moved within the same face or we moved to an adjoined face on the 2D net
+                return (next, bearing);
+            }
+
+            // we've fallen off a face, so we need to work out which face we end up on and which direction we're now facing
+            (int Column, int Row) currentFace = (current.Y / 50, current.X / 50);
+
+            // unreachable states are commented out because they will always return next as a valid move, but shown for completeness
+            switch (currentFace)
+            {
+                case (0, 1): // top
+                    switch (bearing)
+                    {
+                        case Bearing.North: // back, left edge, heading east
+                            return ((0, current.X + 100), Bearing.East);
+                        //case Bearing.South: // front, top edge, heading south
+                        //    return (next, bearing);
+                        //case Bearing.East: // right, left edge, heading east
+                        //    return (next, bearing);
+                        case Bearing.West: // left, left edge, heading east
+                            return ((0, 149 - current.Y), Bearing.East);
+                    }
+                    break;
+                case (0, 2): // right
+                    switch (bearing)
+                    {
+                        case Bearing.North: // back, bottom edge, heading north
+                            return ((current.X - 100, 199), Bearing.North);
+                        case Bearing.South: // front, right edge, heading west
+                            return ((99, current.X - 50), Bearing.West);
+                        case Bearing.East: // bottom, right edge, heading west
+                            return ((99, 149 - current.Y), Bearing.West);
+                        //case Bearing.West: // top, right edge, heading west
+                        //    return (next, bearing);
+                    }
+                    break;
+                case (1, 1): // front
+                    switch (bearing)
+                    {
+                        //case Bearing.North: // top, bottom edge, heading north
+                        //    return (next, bearing);
+                        //case Bearing.South: // bottom, top edge, heading south
+                        //    return (next, bearing);
+                        case Bearing.East: // right, bottom edge, heading north
+                            return ((current.Y + 50, 49), Bearing.North);
+                        case Bearing.West: // left, top edge, heading south
+                            return ((current.Y - 50, 100), Bearing.South);
+                    }
+                    break;
+                case (2, 0): // left
+                    switch (bearing)
+                    {
+                        case Bearing.North: // front, left edge, heading east
+                            return ((50, current.X + 50), Bearing.East);
+                        //case Bearing.South: // back, top edge, heading south
+                        //    return (next, bearing);
+                        //case Bearing.East: // bottom, left edge, heading east
+                        //    return (next, bearing);
+                        case Bearing.West: // top, left edge, heading east
+                            return ((50, 149 - current.Y), Bearing.East);
+                    }
+                    break;
+                case (2, 1): // bottom
+                    switch (bearing)
+                    {
+                        //case Bearing.North: // front, bottom edge, heading north
+                        //    return (next, bearing);
+                        case Bearing.South: // back, right edge, heading west
+                            return ((49, current.X + 100), Bearing.West);
+                        case Bearing.East: // right, right edge, heading west
+                            return ((149, 149 - current.Y), Bearing.West);
+                        //case Bearing.West: // left, right edge, heading west
+                        //    return (next, bearing);
+                    }
+                    break;
+                case (3, 0): // back
+                    switch (bearing)
+                    {
+                        //case Bearing.North: // left, bottom edge, heading north
+                        //    return (next, bearing);
+                        case Bearing.South: // right, top edge, heading south
+                            return ((current.X + 100, 0), bearing);
+                        case Bearing.East: // bottom, bottom edge, heading north
+                            return ((current.Y - 100, 149), Bearing.North);
+                        case Bearing.West: // top, top edge, heading south
+                            return ((current.Y - 100, 0), Bearing.South);
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException($"There is no face on row {currentFace.Row} and column {currentFace.Column}");
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(bearing), bearing, $"Tried to make a weird move from {current}");
+        }
+
+        /// <summary>
+        /// Calculate the final score depending on where you finish and what direction you're facing
+        /// </summary>
+        /// <param name="bearing">Final bearing</param>
+        /// <param name="location">Final location</param>
+        /// <returns>Score</returns>
+        private static int CalculateScore(Bearing bearing, Point2D location)
+        {
+            int bearingValue = bearing switch
+            {
+                Bearing.North => 3,
+                Bearing.South => 1,
+                Bearing.East => 0,
+                Bearing.West => 2,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            return (location.Y + 1) * 1000 + (location.X + 1) * 4 + bearingValue;
+        }
+
+        private abstract record Instruction;
+
+        private record Step(int Count) : Instruction;
+
+        private record Turn(TurnDirection Direction) : Instruction;
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day22Tests.cs
@@ -0,0 +1,100 @@
+using System.IO;
+using AdventOfCode.Utilities;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day22Tests
+    {
+        private static readonly char[,] Grid = GetRealInput()[..^2].ToGrid();
+
+        private readonly ITestOutputHelper output;
+        private readonly Day22 solver;
+
+        public Day22Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day22();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day22.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 56372;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 22 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 197047;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 22 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+
+        [InlineData("top/north -> back/left/east", 92, 0, Bearing.North, 0, 192, Bearing.East)]
+        [InlineData("top/south -> front/top/south", 92, 49, Bearing.South, 92, 50, Bearing.South)]
+        [InlineData("top/east -> right/left/east", 99, 42, Bearing.East, 100, 42, Bearing.East)]
+        [InlineData("top/west -> left/left/east", 50, 42, Bearing.West, 0, 107, Bearing.East)]
+        [InlineData("top/west/cornertop -> left/left/east", 50, 0, Bearing.West, 0, 149, Bearing.East)]
+        [InlineData("top/west/cornerbottom -> left/left/east", 50, 49, Bearing.West, 0, 100, Bearing.East)]
+
+        [InlineData("right/north -> back/bottom/north", 142, 0, Bearing.North, 42, 199, Bearing.North)]
+        [InlineData("right/south -> front/right/west", 142, 49, Bearing.South, 99, 92, Bearing.West)]
+        [InlineData("right/east -> bottom/right/west", 149, 42, Bearing.East, 99, 107, Bearing.West)]
+        [InlineData("right/east/cornertop -> bottom/right/west", 149, 0, Bearing.East, 99, 149, Bearing.West)]
+        [InlineData("right/east/cornerbottom -> bottom/right/west", 149, 49, Bearing.East, 99, 100, Bearing.West)]
+        [InlineData("right/west -> top/right/west", 100, 42, Bearing.West, 99, 42, Bearing.West)]
+            
+        [InlineData("front/north -> top/bottom/north", 92, 50, Bearing.North, 92, 49, Bearing.North)]
+        [InlineData("front/south -> bottom/top/south", 92, 99, Bearing.South, 92, 100, Bearing.South)]
+        [InlineData("front/east -> right/bottom/north", 99, 92, Bearing.East, 142, 49, Bearing.North)]
+        [InlineData("front/west -> left/top/south", 50, 92, Bearing.West, 42, 100, Bearing.South)]
+
+        [InlineData("left/north -> front/left/east", 42, 100, Bearing.North, 50, 92, Bearing.East)]
+        [InlineData("left/south -> back/top/south", 42, 149, Bearing.South, 42, 150, Bearing.South)]
+        [InlineData("left/east -> bottom/left/east", 49, 142, Bearing.East, 50, 142, Bearing.East)]
+        [InlineData("left/west -> top/left/east", 0, 142, Bearing.West, 50, 7, Bearing.East)]
+        [InlineData("left/west/cornertop -> top/left/east", 0, 100, Bearing.West, 50, 49, Bearing.East)]
+        [InlineData("left/west/cornerbottom -> top/left/east", 0, 149, Bearing.West, 50, 0, Bearing.East)]
+
+        [InlineData("bottom/north -> front/bottom/north", 92, 100, Bearing.North, 92, 99, Bearing.North)]
+        [InlineData("bottom/south -> back/right/west", 92, 149, Bearing.South, 49, 192, Bearing.West)] 
+        [InlineData("bottom/east -> right/right/west", 99, 142, Bearing.East, 149, 7, Bearing.West)]
+        [InlineData("bottom/east/cornertop -> right/right/west", 99, 100, Bearing.East, 149, 49, Bearing.West)]
+        [InlineData("bottom/east/cornerbottom -> right/right/west", 99, 149, Bearing.East, 149, 0, Bearing.West)]
+        [InlineData("bottom/west -> left/right/west", 50, 142, Bearing.West, 49, 142, Bearing.West)]
+
+        [InlineData("back/north -> left/bottom/north", 42, 150, Bearing.North, 42, 149, Bearing.North)]
+        [InlineData("back/south -> right/top/south", 42, 199, Bearing.South, 142, 0, Bearing.South)]
+        [InlineData("back/east -> bottom/bottom/north", 49, 192, Bearing.East, 92, 149, Bearing.North)]
+        [InlineData("back/west -> top/top/south", 0, 192, Bearing.West, 92, 0, Bearing.South)]
+
+        public void MoveCube_WhenCalled_MovesCorrectly(string comment, int initialX, int initialY, Bearing initialBearing, int expectedX, int expectedY, Bearing expectedBearing)
+        {
+            Point2D start = (initialX, initialY);
+
+            (Point2D end, Bearing bearing) actual = Day22.MoveCube(start, initialBearing, Grid);
+
+            actual.Should().Be((new Point2D(expectedX, expectedY), expectedBearing), comment);
+        }
+    }
+}
